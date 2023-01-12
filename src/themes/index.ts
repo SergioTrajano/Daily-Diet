c910844a6ba917668008338495680bc75d0605b2
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-12 19:32:51 -0300

    :lipstick: feat: create a theme

---
+++ src/themes/index.ts
@@ -0,0 +1,41 @@
+export default {
+    COLOURS: {
+        GREEN_LIGHT: "#E5F0DB",
+        GREEN_MID: "#CBE4B4",
+        GREEN_DARK: "#639339",
+
+        GRAY_100: "#1B1D1E",
+        GRAY_200: "#333638",
+        GRAY_400: "#B9BBBC",
+        GRAY_500: "#DDDEDF",
+        GRAY_600: "#EFF0F0",
+        GRAY_700: "#FAFAFA",
+
+        RED_LIGHT: "#F4E6E7",
+        RED_MID: "#F3BABD",
+        RED_DARK: "#BF3B44",
+
+        WHITE: "#FFFFFF",
+    },
+
+    FONT_SIZE: {
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24,
+        XL2: 32,
+    },
+
+    LINE_HEIGHT: {
+        SM: 18.2,
+        MD: 20.8,
+        LG: 23.4,
+        XL: 31.2,
+        XL2: 41.6,
+    },
+
+    FONT_FAMILY: {
+        REGULAR: "NunitoSans_400Regular",
+        BOLD: "NunitoSans_700Bold",
+    },
+};
