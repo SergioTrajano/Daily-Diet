e31aecff716ff2ab63df6bebe594f28155a2a270
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-13 02:59:18 -0300

    :sparkles: feat: add type of routes params

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,15 @@
+export declare global {
+    namespace ReactNavigation {
+        type RootParamsList = {
+            home: undefined;
+            newMeal: undefined;
+            motivationalMessage: {
+                isInDiet: boolean;
+            };
+            meal: {
+                mealId: number;
+            };
+            statistics: undefined;
+        };
+    }
+}
