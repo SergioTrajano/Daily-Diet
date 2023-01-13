ca7415c2bab52124800ade17d0c444241741ab31
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-13 03:00:05 -0300

    :sparkles: feat: implement Routes components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import { ThemeProvider } from "styled-components/native";
 
 import theme from "@themes/index";
+import { Routes } from "./src/routes";
 
 export default function App() {
-    return <ThemeProvider theme={theme}></ThemeProvider>;
+    return (
+        <ThemeProvider theme={theme}>
+            <Routes />
+        </ThemeProvider>
+    );
 }
