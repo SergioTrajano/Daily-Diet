a3840c2408274822d57b428379c40502f81134d4
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-14 18:46:22 -0300

    :hammer: refactor: update component to use FlatList

--- src/components/MealCard/index.tsx
+++ src/components/MealCard/index.tsx
@@ -1,19 +1,55 @@
-import { View } from "react-native";
-import { Container, Circle, HourText, MealText, TextDivider } from "./style";
+import { FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import {
+    Container,
+    Circle,
+    HourText,
+    MealText,
+    TextDivider,
+    MealInfoWrapper,
+    Title,
+} from "./style";
 
 type Props = {
+    title: string;
+    data: mealProps[];
+};
+
+type mealProps = {
+    id: number;
     hour: string;
     meal: string;
     isInDiet: boolean;
 };
 
-export function MealCard({ hour, meal, isInDiet = true, ...rest }: Props) {
+export function MealCard({ title, data, ...rest }: Props) {
+    const { navigate } = useNavigation<NativeStackNavigationProp<ReactNavigation.RootParamsList>>();
+
+    function handleMeal(id: number) {
+        navigate("meal", { mealId: id });
+    }
+
     return (
-        <Container {...rest}>
-            <HourText>{hour}</HourText>
-            <TextDivider></TextDivider>
-            <MealText>{meal}</MealText>
-            <Circle isInDiet={isInDiet} />
+        <Container>
+            <FlatList
+                data={data}
+                keyExtractor={(item: mealProps, i: number) => `${item} + ${i}`}
+                renderItem={({ item }: { item: mealProps }) => (
+                    <MealInfoWrapper
+                        onPress={() => handleMeal(item.id)}
+                        {...rest}
+                    >
+                        <HourText>{item.hour}</HourText>
+                        <TextDivider></TextDivider>
+                        <MealText>{item.meal}</MealText>
+                        <Circle isInDiet={item.isInDiet} />
+                    </MealInfoWrapper>
+                )}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={() => <Title>{title}</Title>}
+            />
         </Container>
     );
 }
