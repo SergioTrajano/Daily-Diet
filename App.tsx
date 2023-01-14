95a227bce9015258746623b4fb08fe9222e12e32
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-14 18:46:39 -0300

    :sparkles: feat: add StatusBar component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import {
+    useFonts,
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
 
 import theme from "@themes/index";
 import { Routes } from "./src/routes";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
     return (
         <ThemeProvider theme={theme}>
-            <Routes />
+            <StatusBar
+                barStyle={"dark-content"}
+                backgroundColor="transparent"
+                translucent
+            />
+            {fontsLoaded ? <Routes /> : <></>}
         </ThemeProvider>
     );
 }
