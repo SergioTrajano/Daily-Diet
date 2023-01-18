1d0a14cdc6ed15651eb5ed71f3dcf94db4577a3a
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-18 02:36:30 -0300

    :hammer: refactor:  refactor storage to otimize formatation

--- src/storage/meals/mealCreate.ts
+++ src/storage/meals/mealCreate.ts
@@ -12,11 +12,43 @@ export async function mealCreate(newMealData: Props) {
 
         const id = storedId ? JSON.parse(storedId) : 0;
 
-        const storage = JSON.stringify([...storedMeals, { ...newMealData, id: id + 1 }]);
+        const storage = [...storedMeals, { ...newMealData, id: id + 1 }];
 
-        await AsyncStorage.setItem(MEALS_COLLECTION, storage);
+        const storageOrderd = orderMealsByDateTime(storage);
+
+        await AsyncStorage.setItem(MEALS_COLLECTION, JSON.stringify(storageOrderd));
         await AsyncStorage.setItem(ID, JSON.stringify(id + 1));
     } catch (error) {
         throw error;
     }
+}
+
+function orderMealsByDateTime(meals: MealStorageDTO[]) {
+    const mealsSortedByDate: MealStorageDTO[] = meals.sort((a, b) => {
+        const dateA: string[] = a.date.split("/");
+        const timeA: string[] = a.hour.split(":");
+
+        const dateB: string[] = b.date.split("/");
+        const timeB: string[] = b.hour.split(":");
+
+        const result =
+            new Date(
+                Number(dateB[2]),
+                Number(dateB[1]),
+                Number(dateB[0]),
+                Number(timeB[0]),
+                Number(timeB[1])
+            ).getTime() -
+            new Date(
+                Number(dateA[2]),
+                Number(dateA[1]),
+                Number(dateA[0]),
+                Number(timeA[0]),
+                Number(timeA[1])
+            ).getTime();
+
+        return result;
+    });
+
+    return mealsSortedByDate;
 }
